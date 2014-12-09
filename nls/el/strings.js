91690296490a645bffe5dcd6b9ed5bda17d3940b
georapbox <georapbox@gmail.com>
2014-12-09 11:39:18 +0200

    Update to Greek locale

--- nls/el/strings.js
+++ nls/el/strings.js
@@ -1,6 +1,6 @@
-// English - root strings
+// Greek Locale
 define({
-    'COMMAND_NAME': 'JS Annotate ενεργοποιημένο',
+    'COMMAND_NAME': 'Ενεργοποίηση του JS Annotate',
     /* DIALOG HEADER */
     'ENABLE_PROMPT_DIALOG_TITLE': 'Ενεργοποίηση του JS Annotate',
     'DISABLE_PROMPT_DIALOG_TITLE': 'Απενεργοποίηση του JS Annotate',
@@ -8,7 +8,7 @@ define({
 	'ENABLE_PROMPT_DIALOG_BODY': 'Απαιτείται επανεκκίνηση του Brackets για να ενεργοποιηθεί το JS Annotate.',
 	'DISABLE_PROMPT_DIALOG_BODY': 'Απαιτείται επανεκκίνηση του Brackets για να απενεργοποιηθεί το JS Annotate.',
     /* DIALOG FOOTER */
-    'ENABLE_DIALOG_OK': 'Ενεργοποίηση και επανεκκίνηση',
-    'DISABLE_DIALOG_OK': 'Απενεργοποίηση και επανεκκίνηση',
+    'ENABLE_DIALOG_OK': 'Ενεργοποίηση και Επανεκκίνηση',
+    'DISABLE_DIALOG_OK': 'Απενεργοποίηση και Επανεκκίνηση',
     'DIALOG_CANCEL': 'Ακύρωση'
 });
